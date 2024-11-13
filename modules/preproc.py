e7ba092e980d63058846f158135f7ebaea754893
Giullia Braga <giubdam@gmail.com>
2024-11-13 18:25:15 -0300

    Preprocessing concluded

--- modules/preproc.py
+++ modules/preproc.py
@@ -1,40 +1,102 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 from utils import load_phishing_dataset
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectFromModel, SelectKBest, f_classif
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import loguniform
+from sklearn import datasets
 
 class PhishingDatasetPreproc():
 
-    def init(self):
+    def __init__(self):
         self.dataset = load_phishing_dataset()
         self.target = 'Result'
 
     def basic_operations(self):
         '''
         Basic preprocessing operations:
-        - Converting target variable values from {-1,1} to {0,1} for use in some models.
-        - Splitting features and target variable.
+        - Converting all data to int, as some models cannot deal with categorical data :(
+        - Converting target variable values from {-1,1} to {0,1} for use in some models
+        - Splitting features and target variable
         '''
         trg = self.target
+        
+        # Converting the entire dataset to int
+        self.dataset = self.dataset.astype(int)
 
         # Converting from {-1,1} to {0,1} for use in some models
-        self.dataset[trg] = self.dataset[trg].astype(int)
         self.dataset.loc[self.dataset[trg] == -1, trg] = 0
 
         # Splitting between features and target variable
         self.X = self.dataset.drop(columns=trg)
         self.y = self.dataset[trg]
 
-    def tree_based_feature_selection(dataset, target = 'Result'):
-        '''
-        Simple tree-based feature selection.
-        '''
-        dataset[target] = dataset['Result'].astype(int)
+        return self.dataset, self.X, self.y
 
-        model = RandomForestClassifier()
-        model.fit(dataset.drop(target, axis=1), dataset[target])
+def feature_selection_pipeline(model, param_grid=dict, fs_method=str, X=pd.DataFrame, y=pd.Series, n_iter=10):
+    '''
+    Creates and runs a pipeline with a specified feature selection method and model.
+    
+    model: model to use in the pipeline (e.g., LogisticRegression())
+    param_grid: dictionary with hyperparameters for both the feature selection and model
+    fs_method: str specifying feature selection ("tree", "pca", "univariate", "l1")
+    n_iter: number of random samples for RandomizedSearchCV
+    '''
+    fs_methods = {
+        "tree": SelectFromModel(estimator=RandomForestClassifier(n_estimators=100)),
+        "pca": PCA(),
+        "univariate": SelectKBest(f_classif),
+        "l1": SelectFromModel(estimator=Lasso(alpha=0.1))
+    }
+    
+    if fs_method in fs_methods.keys():
+        print(fs_method)
+        feature_selector = fs_methods[fs_method]
+        print(feature_selector)
+    else:
+        raise ValueError("Unknown feature selection method")
         
-        feature_importances = pd.Series(model.feature_importances_, index=dataset.drop(target, axis=1).columns)
-        selected_features = feature_importances[feature_importances > 0.01].index
+    pipeline = Pipeline([
+        ("selector", feature_selector), 
+        ("model", model)
+    ])
+    
+    search = RandomizedSearchCV(pipeline, param_grid, n_iter=n_iter, n_jobs=2, random_state=42, verbose=1)
 
-        return selected_features
+    search.fit(X,y)
+    
+    # Return best score and params
+    return search.best_score_, search.best_params_
+
+def eval_feature_selectors(model, param_grids, X, y, n_iter=10):
+    '''
+    Evaluates multiple feature selection methods and returns a DataFrame with the results.
+    
+    model: model to use in the pipeline (e.g., LogisticRegression())
+    param_grids: list of dictionaries with hyperparameters for each feature selection and model
+    X: DataFrame with features
+    y: Series or array with target variable
+    n_iter: number of random samples for RandomizedSearchCV
+    '''
+    selectors = ["tree", "pca", "univariate", "l1"]
+    results = []
+
+    for selector, param_grid in zip(selectors, param_grids):
+        best_score, best_params = feature_selection_pipeline(
+            model=model,
+            param_grid=param_grid,
+            fs_method=selector,
+            X=X,
+            y=y,
+            n_iter=n_iter
+        )
+        results.append({"selector_name": selector, "best_cv_score": best_score, "best_params": best_params})
+
+    return pd.DataFrame(results)
+        
+    
 
--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,9 @@
+from matplotlib import pyplot as plt
 import pandas as pd
+from os import X_OK
+from sklearn.decomposition import PCA
+import seaborn as sns
+import umap
 
 
 def load_phishing_dataset(filepath = '../data/phishing-dataset.arff'):
@@ -16,3 +21,78 @@ def load_phishing_dataset(filepath = '../data/phishing-dataset.arff'):
     dataset = pd.DataFrame(data, columns=col_names)
 
     return dataset
+
+def quick_stats(dataset):
+    '''
+    Runs a quick statistical summary of the dataset
+    '''
+    stats = dataset.describe().T
+
+    # Unique value counts
+    value_counts = dataset.apply(lambda col: col.value_counts()).T
+
+    # Output containin the stats and the counts for each unique value
+    stats = (
+                pd.concat([stats,value_counts],axis=1)
+                .fillna(0)
+                .style
+                .format(precision=0)
+            )
+
+    return stats
+
+def plot_pca(dataset, target='Result'):
+    '''
+    Generates a 2D visualization of a dataset using PCA.
+    '''
+    pca = PCA(2)
+
+    X = dataset.drop(columns=target)
+    y = dataset[target]
+
+    pca_result = pca.fit_transform(X)
+
+    dataset_pca = pd.DataFrame({
+        'X': pca_result[:, 0],
+        'y': pca_result[:, 1],
+        'target': y
+    })
+
+    fig = plt.figure(figsize=(22, 8))
+
+    sns.scatterplot(data=dataset_pca, x='X', y='y', hue='target', palette='viridis', s=100, edgecolor='k')
+
+    plt.title('PCA', fontsize=20)
+    plt.xlabel('Principal Component 1', fontsize=16)
+    plt.ylabel('Principal Component 2', fontsize=16)
+    plt.legend(title=target, title_fontsize='13', fontsize='11')
+
+    plt.show()
+
+def plot_umap(dataset, target='Result'):
+    '''    
+    Generates a 2D visualization of a dataset using UMAP (supervised).
+    '''
+    X = dataset.drop(columns=target)
+    y = dataset[target]
+
+    reducer = umap.UMAP()
+    umap_result = reducer.fit_transform(X,y=y)
+
+    dataset_umap = pd.DataFrame({
+        'X': umap_result[:, 0],
+        'y': umap_result[:, 1],
+        'target': y
+    })
+
+
+    fig = plt.figure(figsize=(22, 8))
+
+    sns.scatterplot(data=dataset_umap, x='X', y='y', hue='target', palette='viridis', s=100, edgecolor='k')
+
+    plt.title('UMAP', fontsize=20)
+    plt.xlabel('Dimension 1', fontsize=16)
+    plt.ylabel('Dimension 2', fontsize=16)
+    plt.legend(title=target, title_fontsize='13', fontsize='11')
+
+    plt.show()
